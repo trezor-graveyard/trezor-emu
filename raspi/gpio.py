a8423609dfafba21855d195d43c67144e9547af2
Pavol Rusnak <stick@gk2.sk>
2012-11-05 21:04:58 +0100

    alter gpio to real state

--- raspi/gpio.py
+++ raspi/gpio.py
@@ -2,19 +2,17 @@
 import time
 import wiringpi
 
-PIN_BTNYES = 11
-PIN_BTNNO  = 10
+PIN_BTNYES = 7
+PIN_BTNNO  = 8
 
 wiringpi.wiringPiSetupSys()
 
-for i in xrange(0, 25):
-    wiringpi.pinMode(i, wiringpi.INPUT)
-# wiringpi.pinMode(PIN_BTNYES, wiringpi.INPUT)
-# wiringpi.pinMode(PIN_BTNNO,  wiringpi.INPUT)
+wiringpi.pinMode(PIN_BTNYES, wiringpi.INPUT)
+wiringpi.pinMode(PIN_BTNNO,  wiringpi.INPUT)
+
+wiringpi.pullUpDnControl(PIN_BTNYES, wiringpi.PUD_DOWN)
+wiringpi.pullUpDnControl(PIN_BTNNO,  wiringpi.PUD_DOWN)
 
 while True:
-    for i in xrange(0, 25): 
-        print i, wiringpi.digitalRead(i),
-    print
-#    print 'yes:', wiringpi.digitalRead(PIN_BTNYES), 'no:', wiringpi.digitalRead(PIN_BTNNO)
+    print 'yes:', wiringpi.digitalRead(PIN_BTNYES), 'no:', wiringpi.digitalRead(PIN_BTNNO)
     time.sleep(0.1)
