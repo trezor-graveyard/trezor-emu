dc5ffb028f95f8cc92931cd86ba12452e1a08fe8
slush <info@bitcoin.cz>
2012-11-15 20:27:41 +0000

    Added SignTx, DebugLinkDecision messages Features message indicates support for DebugLink connection

--- bitkey_proto/mapping.py
+++ bitkey_proto/mapping.py
@@ -22,6 +22,12 @@ map_type_to_class = {
        18: proto.PinRequest,
        19: proto.PinAck,
        20: proto.PinCancel,
+       21: proto.SignInput,
+       22: proto.SignedInput,
+       23: proto.TxInput,
+       24: proto.TxOutput,
+       25: proto.SetMaxFeeKb,
+       100: proto.DebugLinkDecision,
 }
 
 map_class_to_type = {}
