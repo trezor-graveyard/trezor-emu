cb8890053351c11e794641279da07777565aa9ed
Pavol Rusnak <stick@gk2.sk>
2012-11-19 12:12:05 +0100

    rework raspi interface

---
+++ raspi/bitkey.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+import time
+import wiringpi
+import spi
+
+class Bitkey:
+
+    def __init__(self):
+        self.PIN_OLED_DC  = 23   # RS
+        self.PIN_OLED_CS  = 24   # CS
+        self.PIN_OLED_RST = 25   # RES
+        self.PIN_BTN_YES = 7
+        self.PIN_BTN_NO  = 8
+        self.OLED_WIDTH   = 128
+        self.OLED_HEIGHT  = 64
+        self.oledbuffer  = [0] * (self.OLED_WIDTH * self.OLED_HEIGHT / 8)
+        self.spidev = None
+
+    def init(self):
+        wiringpi.wiringPiSetupGpio()
+        self.spidev = spi.SPIDev('/dev/spidev0.0')
+
+        wiringpi.pinMode(self.PIN_OLED_DC,  wiringpi.OUTPUT)
+        wiringpi.pinMode(self.PIN_OLED_CS,  wiringpi.OUTPUT)
+        wiringpi.pinMode(self.PIN_OLED_RST, wiringpi.OUTPUT)
+
+        wiringpi.pinMode(self.PIN_BTN_YES, wiringpi.INPUT)
+        wiringpi.pinMode(self.PIN_BTN_NO,  wiringpi.INPUT)
+
+        wiringpi.pullUpDnControl(self.PIN_BTN_YES, wiringpi.PUD_DOWN)
+        wiringpi.pullUpDnControl(self.PIN_BTN_NO,  wiringpi.PUD_DOWN)
+
+        wiringpi.digitalWrite(self.PIN_OLED_DC, 0)
+        wiringpi.digitalWrite(self.PIN_OLED_RST, 0)
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
+
+        wiringpi.digitalWrite(self.PIN_OLED_RST, 1)
+        time.sleep(0.001)
+        wiringpi.digitalWrite(self.PIN_OLED_RST, 0)
+        time.sleep(0.010)
+        wiringpi.digitalWrite(self.PIN_OLED_RST, 1)
+
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
+        seq = [ 0xAE, 0xD5, 0x80, 0xA8, 0x3F, 0xD3, 0x00, 0x40, 0x8D, 0x14, 0x20, 0x00, 0xA1, 0xC8, 0xDA, 0x12, 0x81, 0xCF, 0xD9, 0xF1, 0xDB, 0x40, 0xA4, 0xA6, 0xAF ]
+        self.sendSPI(seq)
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
+
+        self.oledClear()
+        self.oledRefresh()
+
+    def sendSPI(self, data):
+        transfer, _, _ = spi.spi_transfer(data, readlen = 0)
+        self.spidev.do_transfers([transfer])
+
+    def oledClear(self):
+        self.oledbuffer  = [0] * (self.OLED_WIDTH * self.OLED_HEIGHT / 8)
+
+    def oledRefresh(self):
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
+        seq = [0x00, 0x10, 0x40]
+        self.sendSPI(seq)
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
+        wiringpi.digitalWrite(self.PIN_OLED_DC, 1) # data
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
+        self.sendSPI(self.oledbuffer)
+        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
+        wiringpi.digitalWrite(self.PIN_OLED_DC, 0) # cmd
+
+    def oledDrawPixel(self, x, y):
+        if (x < 0) or (y < 0) or (x >= self.OLED_WIDTH) or (y >= self.OLED_HEIGHT):
+            return
+        self.oledbuffer[x+(y/8)*self.OLED_WIDTH] |= (1 << (y%8))
+
+    def oledClearPixel(self, x, y):
+        if (x < 0) or (y < 0) or (x >= self.OLED_WIDTH) or (y >= self.OLED_HEIGHT):
+            return
+        self.oledbuffer[x+(y/8)*self.OLED_WIDTH] &= ~(1 << (y%8))
+
+    def oledDrawChar(self, x, y, c, font):
+        if (x >= self.OLED_WIDTH) or (y >= self.OLED_HEIGHT):
+            return
+        column = [0] * font.OLED_WIDTH
+        if (c >= font.firstchar) and (c <= font.lastchar):
+            for col in xrange(font.OLED_WIDTH):
+                column[col] = font.table[((c - font.firstchar) * font.OLED_WIDTH) + col]
+        else:
+            for col in xrange(font.OLED_WIDTH):
+                column[col] = 0xFF
+        for xoffset in xrange(font.OLED_WIDTH):
+            for yoffset in xrange(font.OLED_HEIGHT+1):
+                bit = column[xoffset] << (8 - (yoffset + 1))
+                bit = bit >> 7
+                if bit > 0:
+                    self.drawPixel(x + xoffset, y + yoffset)
+
+    def oledDrawString(self, x, y, text, font):
+        for i in xrange(len(text)):
+            self.drawChar(x + (i * (font.OLED_WIDTH + 1)), y, ord(text[i]), font)
+
+    def oledInvert(self, x1, y1, x2, y2):
+        if (x1 >= self.OLED_WIDTH) or (y1 >= self.OLED_HEIGHT) or (x2 >= self.OLED_WIDTH) or (y2 >= self.OLED_HEIGHT):
+            return
+        for x in xrange(x1, x2+1):
+            for y in xrange(y1, y2+1):
+                self.oledbuffer[x+(y/8)*self.OLED_WIDTH] ^= (1 << (y%8))
+
+    def buttonsRead(self):
+        return [ wiringpi.digitalRead(self.PIN_BTN_NO) == 0 , wiringpi.digitalRead(self.PIN_BTN_YES) == 0 ]
