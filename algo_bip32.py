14c1973a269b4fa96059cf61d384154d347d8f99
slush <info@bitcoin.cz>
2012-11-24 04:57:31 +0000

    Fixed "Not implemented" error

--- algo_bip32.py
+++ algo_bip32.py
@@ -2,16 +2,16 @@ class AlgoBIP32(object):
     
     @classmethod
     def init_master_private_key(cls, seed):
-        raise NotImplemented
+        raise NotImplemented("Not implemented")
     
     @classmethod
     def init_master_public_key(cls, seed):
-        raise NotImplemented
+        raise NotImplemented("Not implemented")
 
     @classmethod
     def get_new_address(cls, seed, n):
-        raise NotImplemented
+        raise NotImplemented("Not implemented")
     
     @classmethod
     def get_private_key(cls, seed, n):
-        raise NotImplemented
+        raise NotImplemented("Not implemented")
