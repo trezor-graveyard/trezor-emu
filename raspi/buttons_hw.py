18f21cbc3b38960911300c8c90499ea6d571c734
Pavol Rusnak <stick@gk2.sk>
2012-11-26 12:35:59 +0100

    drop wiringpi dep

--- raspi/buttons_hw.py
+++ raspi/buttons_hw.py
@@ -1,19 +1,19 @@
-import wiringpi
-
-
 class HwButtons(object):
     def __init__(self):
         self.PIN_BTN_YES = 8
         self.PIN_BTN_NO  = 7
-        
+
+    def __readPin(self, pin):
+        with open('/sys/class/gpio/gpio%d/value' % pin, 'r') as f:
+            return f.read(1)
+
     def read(self):
-        no_state = wiringpi.digitalRead(self.PIN_BTN_NO) > 0
-        yes_state = wiringpi.digitalRead(self.PIN_BTN_YES) > 0
-        
+        no_state = (self.__readPin(self.PIN_BTN_NO) == '1')
+        yes_state = (self.__readPin(self.PIN_BTN_YES) == '1')
+
         if no_state:
             return False
         if yes_state:
             return True
-        
+
         return None
-        
--- raspi/display_spi.py
+++ raspi/display_spi.py
@@ -1,5 +1,4 @@
 import time
-import wiringpi
 import spidev
 
 class SPIDisplay(object):
@@ -11,47 +10,40 @@ class SPIDisplay(object):
         self.spidev = None
 
     def init(self):
-        wiringpi.wiringPiSetupGpio()
         self.spidev = spidev.SpiDev()
         self.spidev.open(0, 0)
 
-        wiringpi.pinMode(self.PIN_OLED_DC,  wiringpi.OUTPUT)
-        wiringpi.pinMode(self.PIN_OLED_CS,  wiringpi.OUTPUT)
-        wiringpi.pinMode(self.PIN_OLED_RST, wiringpi.OUTPUT)
+        self.__writePin(self.PIN_OLED_DC, 0)
+        self.__writePin(self.PIN_OLED_RST, 0)
+        self.__writePin(self.PIN_OLED_CS, 1) # deselect
 
-        wiringpi.pinMode(self.PIN_BTN_YES, wiringpi.INPUT)
-        wiringpi.pinMode(self.PIN_BTN_NO,  wiringpi.INPUT)
-
-        wiringpi.pullUpDnControl(self.PIN_BTN_YES, wiringpi.PUD_DOWN)
-        wiringpi.pullUpDnControl(self.PIN_BTN_NO,  wiringpi.PUD_DOWN)
-
-        wiringpi.digitalWrite(self.PIN_OLED_DC, 0)
-        wiringpi.digitalWrite(self.PIN_OLED_RST, 0)
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
-
-        wiringpi.digitalWrite(self.PIN_OLED_RST, 1)
+        self.__writePin(self.PIN_OLED_RST, 1)
         time.sleep(0.001)
-        wiringpi.digitalWrite(self.PIN_OLED_RST, 0)
+        self.__writePin(self.PIN_OLED_RST, 0)
         time.sleep(0.010)
-        wiringpi.digitalWrite(self.PIN_OLED_RST, 1)
+        self.__writePin(self.PIN_OLED_RST, 1)
 
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
+        self.__writePin(self.PIN_OLED_CS, 0) # select
         seq = [ 0xAE, 0xD5, 0x80, 0xA8, 0x3F, 0xD3, 0x00, 0x40, 0x8D, 0x14, 0x20, 0x00, 0xA1, 0xC8, 0xDA, 0x12, 0x81, 0xCF, 0xD9, 0xF1, 0xDB, 0x40, 0xA4, 0xA6, 0xAF ]
-        self._sendSPI(seq)
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
+        self.__sendSPI(seq)
+        self.__writePin(self.PIN_OLED_CS, 1) # deselect
 
         self.render()
 
-    def _sendSPI(self, data):
+    def __sendSPI(self, data):
         self.spidev.xfer2(data)
 
+    def __writePin(self, pin, value):
+        with open('/sys/class/gpio/gpio%d/value' % pin, 'r') as f:
+            f.write(value)
+
     def refresh(self):
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
+        self.__writePin(self.PIN_OLED_CS, 0) # select
         seq = [0x00, 0x10, 0x40]
-        self._sendSPI(seq)
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
-        wiringpi.digitalWrite(self.PIN_OLED_DC, 1) # data
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 0) # select
-        self._sendSPI(self.buffer.data)
-        wiringpi.digitalWrite(self.PIN_OLED_CS, 1) # deselect
-        wiringpi.digitalWrite(self.PIN_OLED_DC, 0) # cmd
+        self.__sendSPI(seq)
+        self.__writePin(self.PIN_OLED_CS, 1) # deselect
+        self.__writePin(self.PIN_OLED_DC, 1) # data
+        self.__writePin(self.PIN_OLED_CS, 0) # select
+        self.__sendSPI(self.buffer.data)
+        self.__writePin(self.PIN_OLED_CS, 1) # deselect
+        self.__writePin(self.PIN_OLED_DC, 0) # cmd
