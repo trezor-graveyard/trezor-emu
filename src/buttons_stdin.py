95de8849c8383270d3c177c006f3b0e8f5342992
slush <info@bitcoin.cz>
2012-11-26 20:28:02 +0000

    Print hint about y/n keys to the console

--- src/buttons_stdin.py
+++ src/buttons_stdin.py
@@ -2,6 +2,10 @@ from select import select # For raw_input timeout
 import sys
 
 class StdinButtons(object):
+    def __init__(self):
+        print "Press y+<enter> to confirm an action."
+        print "Press n+<enter> to cancel an action."
+        
     def read(self):
         rlist, _, _ = select([sys.stdin], [], [], 0)
         if not rlist:                
