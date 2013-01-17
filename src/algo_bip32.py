19c631686577136a845885e9288881029a4f08d7
slush <info@bitcoin.cz>
2013-01-17 14:10:04 +0100

    Interface accepts secexp instead of seed

--- src/algo_bip32.py
+++ src/algo_bip32.py
@@ -1,17 +1,17 @@
 class AlgoBIP32(object):
     
     @classmethod
-    def init_master_private_key(cls, seed):
+    def init_master_private_key(cls, secexp):
         raise NotImplemented("Not implemented")
     
     @classmethod
-    def init_master_public_key(cls, seed):
+    def init_master_public_key(cls, secexp):
         raise NotImplemented("Not implemented")
 
     @classmethod
-    def get_new_address(cls, seed, n):
+    def get_new_address(cls, secexp, n):
         raise NotImplemented("Not implemented")
     
     @classmethod
-    def get_private_key(cls, seed, n):
+    def get_private_key(cls, secexp, n):
         raise NotImplemented("Not implemented")
