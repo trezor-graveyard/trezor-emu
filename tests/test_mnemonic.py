97a4cb95f6b835e7ba365fca3c77c109390970ca
slush <info@bitcoin.cz>
2013-02-06 19:28:20 +0100

    Basic mnemonic test

--- tests/test_mnemonic.py
+++ tests/test_mnemonic.py
@@ -1,9 +1,19 @@
 import unittest
 
-class TestMnemonic(unittest.TestCase):
-    def test_features(self):
-        pass
+import sys
+sys.path.append('../src')
 
+import mnemonic
+
+mnem = 'beyond neighbor scratch swirl embarrass doll cause also stick softly physical nice'.split(' ')
+seed = '13ee91e3c7e49dd620a5dd5a51d9d766'
+
+class TestMnemonic(unittest.TestCase):
+    def test_decode(self):
+        self.assertEqual(mnemonic.decode(mnem), seed)
+
+    def test_encode(self):
+        self.assertEqual(mnemonic.encode(seed), mnem)
 
 if __name__ == '__main__':
     unittest.main()
