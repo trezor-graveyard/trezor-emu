f2cb746011d11deadedab824fb0ebbe1c063c5f1
Pavol Rusnak <stick@gk2.sk>
2013-03-11 13:43:54 +0100

    add untracked files

---
+++ bitkey-rpi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Put following line to rc.local:
+# screen -AmdS bitkey bash -i /home/pi/bitkey-python/bitkey-rpi.sh
+
+cd `dirname $0`
+
+./setup_hw.sh
+
+./src/bitkey.py -s -t socket -p 0.0.0.0:2000
