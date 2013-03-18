01b6b458ab138728bfda29726da379b0005057cc
Pavol Rusnak <stick@gk2.sk>
2013-03-18 16:27:10 +0100

    fixes in docu

--- README.md
+++
@@ -1,4 +0,0 @@
-bitkey-python
-=============
-
-Python implementation of Trezor-compatible Bitcoin hardware wallets. See http://trezor.bitcoin.cz for more information.
--- bitkey-rpi-eth.sh
+++ bitkey-rpi-eth.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# add the following line to /etc/rc.local to invoke the script automatically on boot
-# screen -AmdS bitkey bash -i /home/pi/bitkey-python/bitkey-rpi-eth.sh
-
 # load kernel modules for spi communication
 modprobe spi_bcm2708
 modprobe spidev
