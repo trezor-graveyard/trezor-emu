7e4df844e9a84fa219aed9ce038312a1bab05039
Pavol Rusnak <stick@gk2.sk>
2013-03-18 16:17:16 +0100

    rename scripts

--- bitkey-rpi-serial.sh
+++ bitkey-rpi-serial.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # add the following line to /etc/rc.local to invoke the script automatically on boot
-# screen -AmdS bitkey bash -i /home/pi/bitkey-python/bitkey-rpi-eth.sh
+# screen -AmdS bitkey bash -i /home/pi/bitkey-python/bitkey-rpi-serial.sh
 
 # load kernel modules for spi communication
 modprobe spi_bcm2708
 modprobe spidev
 
+# kill getty process interfering with serial port
+pkill -f '.*getty.*ttyAMA0.*'
+
 # chdir into script location
 cd `dirname $0`
 
 # run bitkey daemon
-./src/bitkey.py -s -t socket -p 0.0.0.0:2000
+./src/bitkey.py -s # (serial transport on /dev/ttyAMA0 is the default)
--- setup_hw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-modprobe spi_bcm2708
-modprobe spidev
-
-# modprobe i2c_bcm2708
-# modprobe i2c_dev
