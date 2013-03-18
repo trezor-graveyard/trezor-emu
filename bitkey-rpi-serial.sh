a4a1543265b2c348e7d201a4f93d4634136a5ced
Pavol Rusnak <stick@gk2.sk>
2013-03-18 16:36:40 +0100

    don't kill, but stop+continue getty

--- bitkey-rpi-serial.sh
+++ bitkey-rpi-serial.sh
@@ -3,11 +3,14 @@
 modprobe spi_bcm2708
 modprobe spidev
 
-# kill getty process interfering with serial port
-pkill -f '.*getty.*ttyAMA0.*'
+# stop getty process interfering with serial port
+pkill -STOP -f '.*getty.*ttyAMA0.*'
 
 # chdir into script location
 cd `dirname $0`
 
 # run bitkey daemon
 ./src/bitkey.py -s # (serial transport on /dev/ttyAMA0 is the default)
+
+# resume getty
+pkill -CONT -f '.*getty.*ttyAMA0.*'
