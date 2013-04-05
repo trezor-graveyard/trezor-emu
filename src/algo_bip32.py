ce92e54d5388fedbb98591f0bc91d5e1564cabe8
slush <info@bitcoin.cz>
2013-04-05 17:10:01 +0200

    get_secexp_from_seed()

--- src/algo_bip32.py
+++ src/algo_bip32.py
@@ -1,5 +1,8 @@
 class AlgoBIP32(object):
-    
+    @classmethod
+    def get_secexp_from_seed(cls, seed):
+        raise NotImplemented("Not implemented")
+        
     @classmethod
     def init_master_private_key(cls, secexp):
         raise NotImplemented("Not implemented")
