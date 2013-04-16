8d5956a8b76f9416e9f746cf2b47f9c1ce7f16d0
Alex Rembish <alex@rembish.org>
2013-04-16 21:05:46 +0200

    Code reorganization, base eggification, system executable

--- tests/test_mnemonic.py
+++ tests/test_mnemonic.py
@@ -1,9 +1,5 @@
 import unittest
-
-import sys
-sys.path.append('../src')
-
-import mnemonic
+from bitkey import mnemonic
 
 mnem = 'beyond neighbor scratch swirl embarrass doll cause also stick softly physical nice'.split(' ')
 seed = '13ee91e3c7e49dd620a5dd5a51d9d766'
