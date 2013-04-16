d13657ed68be5a1f07a70e7c5a71329d1a2f4a54
Alex Rembish <alex@rembish.org>
2013-04-16 21:59:39 +0200

    PEP8ized (max row length is 120)

--- bitkey/display_virtual.py
+++ bitkey/display_virtual.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 import os
 import pygame
-from display_buffer import DisplayBuffer 
-        
+
+
 class VirtualDisplay(object):
     def __init__(self, buffer):
         self.buffer = buffer
         self.black = pygame.Color(0, 0, 0)
-        self.white = pygame.Color(255,255,255)
+        self.white = pygame.Color(255, 255, 255)
         self.scale = 2
-        
+
         self.drivers = ['x11', 'dga', 'directfb', 'fbcon', 'ggi', 'vgl', 'svgalib', 'aalib']
         self.screen = None
         self.surface = None
         self.frameno = 0
-        
+
     def _select_driver(self):
         for driver in self.drivers:
             os.putenv('SDL_VIDEODRIVER', driver)
@@ -24,7 +24,7 @@ class VirtualDisplay(object):
                 print 'Driver: {0} failed.'.format(driver)
                 continue
             return driver
-    
+
     def init(self):
         driver = self._select_driver()
         if driver == 'x11':
@@ -38,26 +38,27 @@ class VirtualDisplay(object):
 
         self.surface = pygame.Surface((self.buffer.width, self.buffer.height))
 
-    def refresh(self):        
+    def refresh(self):
         self.surface.fill(self.black)
-        
+
         set_at = self.surface.set_at
         white = self.white
         width = self.buffer.width
         height = self.buffer.height
         data = self.buffer.data
-        
+
         '''Optimized for performance. Especially inline writedown of get_pixel helped a lot'''
         for x in range(width):
             for y in range(height):
                 #pix = get_pixel(x, y)
-                pix = data[x+(y/8)*width] & (1 << (y%8)) > 0
-                if pix: set_at((x, y), white)
-           
+                pix = data[x + (y / 8) * width] & (1 << (y % 8)) > 0
+                if pix:
+                    set_at((x, y), white)
+
         pygame.transform.scale(self.surface, self.screen.get_size(), self.screen)
         pygame.display.flip()
 
         # uncomment the following line to take screenshot on every refresh
         # pygame.image.save(self.screen, "frame%06d.png" % self.frameno)
-        
+
         self.frameno += 1
