282c016662379c6b7576de9d330214b6acf87e74
slush <info@bitcoin.cz>
2013-07-03 00:03:01 +0000

    Init script for RPi

--- rpi-autorun.sh
+++ rpi-autorun.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#
-# Put following line to /etc/rc.local to start trezor-rpi on system boot:
-#
-# screen -AmdS trezor bash -i /home/pi/trezor/rpi-autorun.sh
-#
 
 cd `dirname $0`
 
 while [ 1 ]
 do
+    echo "Updating sources from Git..."
     git pull
+
+    echo "Starting trezor-emu..."
     sh rpi-serial.sh
     sleep 2
 done
