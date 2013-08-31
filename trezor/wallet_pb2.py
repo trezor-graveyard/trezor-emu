6790e224a5844a6e7e70e5d301c9687e1c84c72b
slush <info@bitcoin.cz>
2013-08-31 23:47:41 +0200

    binary wallet protobuf reflects latest changes

--- trezor/wallet_pb2.py
+++ trezor/wallet_pb2.py
@@ -12,7 +12,7 @@ import trezor_pb2
 DESCRIPTOR = descriptor.FileDescriptor(
   name='wallet.proto',
   package='',
-  serialized_pb='\n\x0cwallet.proto\x1a\x0ctrezor.proto\"a\n\x06Wallet\x12\x18\n\x04\x61lgo\x18\x01 \x02(\x0e\x32\n.Algorithm\x12\x0e\n\x06secexp\x18\x02 \x02(\x0c\x12\x0f\n\x07has_otp\x18\x03 \x01(\x08\x12\x0f\n\x07has_spv\x18\x04 \x01(\x08\x12\x0b\n\x03pin\x18\x05 \x01(\x0c')
+  serialized_pb='\n\x0cwallet.proto\x1a\x0ctrezor.proto\"#\n\x06Wallet\x12\x0c\n\x04seed\x18\x01 \x02(\x0c\x12\x0b\n\x03pin\x18\x02 \x01(\x0c')
 
 
 
@@ -25,36 +25,15 @@ _WALLET = descriptor.Descriptor(
   containing_type=None,
   fields=[
     descriptor.FieldDescriptor(
-      name='algo', full_name='Wallet.algo', index=0,
-      number=1, type=14, cpp_type=8, label=2,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='secexp', full_name='Wallet.secexp', index=1,
-      number=2, type=12, cpp_type=9, label=2,
+      name='seed', full_name='Wallet.seed', index=0,
+      number=1, type=12, cpp_type=9, label=2,
       has_default_value=False, default_value="",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
-      name='has_otp', full_name='Wallet.has_otp', index=2,
-      number=3, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='has_spv', full_name='Wallet.has_spv', index=3,
-      number=4, type=8, cpp_type=7, label=1,
-      has_default_value=False, default_value=False,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    descriptor.FieldDescriptor(
-      name='pin', full_name='Wallet.pin', index=4,
-      number=5, type=12, cpp_type=9, label=1,
+      name='pin', full_name='Wallet.pin', index=1,
+      number=2, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value="",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -69,10 +48,9 @@ _WALLET = descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=30,
-  serialized_end=127,
+  serialized_end=65,
 )
 
-_WALLET.fields_by_name['algo'].enum_type = trezor_pb2._ALGORITHM
 DESCRIPTOR.message_types_by_name['Wallet'] = _WALLET
 
 class Wallet(message.Message):
