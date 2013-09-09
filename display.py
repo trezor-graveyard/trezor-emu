33c402c7fc959e7152cbc4d66db58e38ca288949
Pavol Rusnak <stick@gk2.sk>
2013-09-09 12:13:48 +0200

    modify display util to show images using PIL

--- display.py
+++ display.py
@@ -2,6 +2,7 @@
 
 import argparse
 import time
+import Image
 
 from trezor.display_buffer import DisplayBuffer
 from trezor.display import Display
@@ -9,9 +10,10 @@ from trezor.layout import Layout
 from trezor import DISPLAY_WIDTH, DISPLAY_HEIGHT
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Show message on SPI display.')
+    parser = argparse.ArgumentParser(description='Show a message or a text on SPI display.')
 
-    parser.add_argument('-t', '--text', dest='text', default='', help='Message to display.Use pipe as a line delimiter.')
+    parser.add_argument('-t', '--text', dest='text', default='', help='Message to display. Use pipe as a line delimiter.')
+    parser.add_argument('-i', '--image', dest='image', default='', help='Filename of image to display.')
     parser.add_argument('-p', '--pygame', dest='pygame', action='store_true', help='Use pygame for rendering')
     return parser.parse_args()
 
@@ -24,7 +26,21 @@ def main():
 
     # Initialize layout driver
     layout = Layout(buff)
-    layout.show_message(args.text.split('|'))
+
+    if args.text:
+      layout.show_message(args.text.split('|'))
+    if args.image:
+      im = Image.open(args.image) # has to be 1-bit-per-pixel PNG (black-white)
+      imd = list(im.getdata())
+      img = [0] * (DISPLAY_WIDTH*DISPLAY_HEIGHT/8)
+      for y in range(DISPLAY_HEIGHT):
+        for x in range(DISPLAY_WIDTH):
+          c = imd[x + y * DISPLAY_WIDTH]
+          if c[0] + c[1] + c[2] > 384:
+            img[(x / 8) + y * DISPLAY_WIDTH / 8] |= (1 << (7 - x % 8))
+
+      layout.show_logo(img)
+
     display.refresh()
 
     if args.pygame:
