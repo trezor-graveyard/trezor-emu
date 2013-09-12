1b5ddd8cadd25f766cca17176da53e58efd495f2
slush0 <info@bitcoin.cz>
2013-09-12 05:58:29 +0200

    Added definitions of coins (currently BTC, tBTC, NMC, LTC)

---
+++ trezor/coindef.py
@@ -0,0 +1,36 @@
+import trezor_pb2 as proto
+
+BTC = proto.CoinType(
+    maxfee_kb=1000000,  # == 0.01 BTC/kB
+    coin_name='Bitcoin',
+    coin_shortcut='BTC',
+    address_type=0,
+)
+
+tBTC = proto.CoinType(
+    maxfee_kb=10000000,  # == 0.1 tBTC/kB
+    coin_name='Testnet',
+    coin_shortcut='tBTC',
+    address_type=111,
+)
+
+NMC = proto.CoinType(
+    maxfee_kb=10000000,  # == 0.1 NMC/kB
+    coin_name='Namecoin',
+    coin_shortcut='NMC',
+    address_type=180,
+)
+
+LTC = proto.CoinType(
+    maxfee_kb=10000000,  # == 0.1 LTC/kB
+    coin_name='Litecoin',
+    coin_shortcut='NMC',
+    address_type=48,
+)
+
+types = {
+    'BTC': BTC,
+    'tBTC': tBTC,
+    'NMC': NMC,
+    'LTC': LTC,
+}
