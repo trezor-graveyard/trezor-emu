1c6bb66205548ba605934528f74739feafda39b7
slush0 <info@bitcoin.cz>
2013-10-08 19:32:49 +0200

    Explicit linking of protoc

--- protob/build.sh
+++ protob/build.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-protoc --python_out=../trezor/ trezor.proto
-protoc --python_out=../trezor/ storage.proto
+protoc --python_out=../trezor/ -I/usr/include -I. trezor.proto
+protoc --python_out=../trezor/ -I/usr/include -I. storage.proto
