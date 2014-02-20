167e5c369388fbceeaf1b135de3429877d1d5e4e
slush0 <slush@satoshilabs.com>
2014-02-20 19:14:18 +0100

    Compiled pb

--- trezor/storage_pb2.py
+++ trezor/storage_pb2.py
@@ -14,7 +14,7 @@ import types_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='storage.proto',
   package='',
-  serialized_pb='\n\rstorage.proto\x1a\x0btypes.proto\"\xbd\x01\n\x07Storage\x12\x0f\n\x07version\x18\x01 \x02(\r\x12\x19\n\x04node\x18\x02 \x01(\x0b\x32\x0b.HDNodeType\x12\x16\n\x08mnemonic\x18\x03 \x01(\x0c\x42\x04\x88\xb5\x18\x01\x12\x1d\n\x15passphrase_protection\x18\x04 \x01(\x08\x12\x1b\n\x13pin_failed_attempts\x18\x05 \x01(\r\x12\x0b\n\x03pin\x18\x06 \x01(\x0c\x12\x10\n\x08language\x18\x07 \x01(\x0c\x12\x13\n\x05label\x18\x08 \x01(\x0c\x42\x04\x88\xb5\x18\x01\">\n\x07Session\x12\x18\n\npassphrase\x18\x01 \x01(\x0c\x42\x04\x88\xb5\x18\x01\x12\x19\n\x04node\x18\x02 \x01(\x0b\x32\x0b.HDNodeTypeB9\n(org.multibit.hd.hardware.trezor.protobufB\rTrezorStorage')
+  serialized_pb='\n\rstorage.proto\x1a\x0btypes.proto\"\xbd\x01\n\x07Storage\x12\x0f\n\x07version\x18\x01 \x02(\r\x12\x19\n\x04node\x18\x02 \x01(\x0b\x32\x0b.HDNodeType\x12\x16\n\x08mnemonic\x18\x03 \x01(\x0c\x42\x04\x88\xb5\x18\x01\x12\x1d\n\x15passphrase_protection\x18\x04 \x01(\x08\x12\x1b\n\x13pin_failed_attempts\x18\x05 \x01(\r\x12\x0b\n\x03pin\x18\x06 \x01(\x0c\x12\x10\n\x08language\x18\x07 \x01(\x0c\x12\x13\n\x05label\x18\x08 \x01(\x0c\x42\x04\x88\xb5\x18\x01\x42\x39\n(org.multibit.hd.hardware.trezor.protobufB\rTrezorStorage')
 
 
 
@@ -95,57 +95,14 @@ _STORAGE = _descriptor.Descriptor(
   serialized_end=220,
 )
 
-
-_SESSION = _descriptor.Descriptor(
-  name='Session',
-  full_name='Session',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='passphrase', full_name='Session.passphrase', index=0,
-      number=1, type=12, cpp_type=9, label=1,
-      has_default_value=False, default_value="",
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=_descriptor._ParseOptions(descriptor_pb2.FieldOptions(), '\210\265\030\001')),
-    _descriptor.FieldDescriptor(
-      name='node', full_name='Session.node', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=222,
-  serialized_end=284,
-)
-
 _STORAGE.fields_by_name['node'].message_type = types_pb2._HDNODETYPE
-_SESSION.fields_by_name['node'].message_type = types_pb2._HDNODETYPE
 DESCRIPTOR.message_types_by_name['Storage'] = _STORAGE
-DESCRIPTOR.message_types_by_name['Session'] = _SESSION
 
 class Storage(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
   DESCRIPTOR = _STORAGE
 
   # @@protoc_insertion_point(class_scope:Storage)
-
-class Session(_message.Message):
-  __metaclass__ = _reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _SESSION
-
-  # @@protoc_insertion_point(class_scope:Session)
 
 
 DESCRIPTOR.has_options = True
@@ -154,6 +111,4 @@ _STORAGE.fields_by_name['mnemonic'].has_options = True
 _STORAGE.fields_by_name['mnemonic']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), '\210\265\030\001')
 _STORAGE.fields_by_name['label'].has_options = True
 _STORAGE.fields_by_name['label']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), '\210\265\030\001')
-_SESSION.fields_by_name['passphrase'].has_options = True
-_SESSION.fields_by_name['passphrase']._options = _descriptor._ParseOptions(descriptor_pb2.FieldOptions(), '\210\265\030\001')
 # @@protoc_insertion_point(module_scope)
